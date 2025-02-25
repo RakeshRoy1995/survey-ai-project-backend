2af900ddd5ea2f1ef7153728d797fc7bd1523678
RakeshRoy1995 <rakeshroyshuvo@gmail.com>
2025-02-25 11:31:03 +0600

    create user resource

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,8 +4,11 @@ import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { CacheModule, CacheInterceptor } from '@nestjs/cache-manager';
 import { APP_INTERCEPTOR } from '@nestjs/core';
-
-@Module({})
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UsersModule } from './users/users.module';
+@Module({
+  imports: [UsersModule]
+})
 export class AppModule {
   static async register() {
     // const { UsersModule } = await import('./users/users.module');
@@ -19,6 +22,16 @@ export class AppModule {
           max: 100, // Maximum number of items in the cache (optional),
           isGlobal: true,
         }),
+        TypeOrmModule.forRoot({
+          type: 'mysql',
+          host: 'localhost',
+          port: 3306,
+          username: 'root',
+          password: '',
+          database: 'survey-project',
+          entities: [__dirname + '/**/*.entity{.ts,.js}'],
+          synchronize: true,
+        })
       ],
       controllers: [AppController],
       providers: [
