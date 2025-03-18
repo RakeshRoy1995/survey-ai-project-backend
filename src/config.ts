8073b900b9865763e1f4bfac8486368f7dd823b1
RakeshRoy1995 <rakeshroyshuvo@gmail.com>
2025-03-19 04:43:37 +0600

    db update

--- src/config.ts
+++ src/config.ts
@@ -1,16 +1,18 @@
 const PORT = 3000;
-const DB_HOST = 'localhost';
-const DB_PORT = 3306;
-const DB_USER = 'root';
-const DB_PASSWORD = '';
-const DB_NAME = 'survey-project';
 
 
-// const DB_HOST = 'database-1.c1ggukwqgrnh.ap-south-1.rds.amazonaws.com';
+// const DB_HOST = 'localhost';
 // const DB_PORT = 3306;
-// const DB_USER = 'admin';
-// const DB_PASSWORD = '##Roy1019876*';
-// const DB_NAME = 'nestjs_db';
+// const DB_USER = 'root';
+// const DB_PASSWORD = '';
+// const DB_NAME = 'survey-project';
+
+
+const DB_HOST = 'database-1.c1ggukwqgrnh.ap-south-1.rds.amazonaws.com';
+const DB_PORT = 3306;
+const DB_USER = 'admin';
+const DB_PASSWORD = '##Roy1019876*';
+const DB_NAME = 'nestjs_db';
 
 
 const JWT_SECRET = '8049076547aa00ca9da1b553f77f79acc9c601cd55e0777b515808e9534a9ed5';
