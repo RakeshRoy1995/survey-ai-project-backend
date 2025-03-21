63a376c0e4d7bf30e4a71e1567f52d1fc457b244
RakeshRoy1995 <rakeshroyshuvo@gmail.com>
2025-03-21 13:53:20 +0600

    DB

--- src/config.ts
+++ src/config.ts
@@ -1,18 +1,18 @@
 const PORT = 3000;
 
 
-const DB_HOST = 'localhost';
-const DB_PORT = 3306;
-const DB_USER = 'root';
-const DB_PASSWORD = '';
-const DB_NAME = 'survey-project';
-
-
-// const DB_HOST = 'database-1.c1ggukwqgrnh.ap-south-1.rds.amazonaws.com';
+// const DB_HOST = 'localhost';
 // const DB_PORT = 3306;
-// const DB_USER = 'admin';
-// const DB_PASSWORD = '##Roy1019876*';
-// const DB_NAME = 'nestjs_db';
+// const DB_USER = 'root';
+// const DB_PASSWORD = '';
+// const DB_NAME = 'survey-project';
+
+
+const DB_HOST = 'database-1.c1ggukwqgrnh.ap-south-1.rds.amazonaws.com';
+const DB_PORT = 3306;
+const DB_USER = 'admin';
+const DB_PASSWORD = '##Roy1019876*';
+const DB_NAME = 'nestjs_db';
 
 
 // const DB_HOST = '149.28.135.157';
